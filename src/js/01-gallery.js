4956e69b218949a99711803a5b9e2799705a8978
Yaroslava <yaroslava.hovenko@gmail.com>
2024-01-07 23:33:26 +0200

    finish with settings

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,18 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// const sum = 10;
+// console.log('const:', sum);
+
+// import { sum as totalSum, multiply } from './02-video';
+// console.log('resultFn:', totalSum(5, 3));
+// console.log('resultFn:', multiply(5, 3));
+
+//якщо більше ніж 5 імпортів з одного файлу то затягуємо весь файл
+import * as data from './02-video';
+console.log(data.sum(2, 3));
+
+//дефолтний іморт
+import value from './02-video';
+console.log(value); // const a = 10
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+function sum(a, b) {
+  return a + b;
+}
+function multiply(a, b) {
+  return a * b;
+}
 
+export { sum, multiply };
+
+///дефолтний експорт один на сторінці (наприклад  забираємо один великий клас (назва ролі не грає ))
+const a = 10;
+
+export default a;
